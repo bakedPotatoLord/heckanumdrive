dcb20719f631aae78438d63da5d85e93d2b3dd3b
bakedPotatoLord <918780@dpsk12.net>
2022-05-20 09:42:20 -0600

    changed drive to spin

--- src/main/java/frc/robot/commands/Spin.java
+++ src/main/java/frc/robot/commands/Spin.java
@@ -5,41 +5,52 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.DriveBase;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
+
+
 /** An example command that uses an example subsystem. */
-public class Drive extends CommandBase {
+public class Spin extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final DriveBase m_subsystem;
+  private final DriveBase m_driveBase;
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public Drive(DriveBase subsystem) {
-    m_subsystem = subsystem;
+  public Spin(DriveBase subsystem) {
+    m_driveBase = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
   }
 
+  Timer spinTimer = new Timer();
+
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    
+    spinTimer.reset();
+    spinTimer.start();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-		
+		m_driveBase.driveCartesian(0, 0, 0.5); 
 	}
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    spinTimer.stop();
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return (spinTimer.get() >= 2); 
   }
 }
