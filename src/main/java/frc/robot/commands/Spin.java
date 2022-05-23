09a1ecc037707e7542e427513e4e21476aff2a76
bakedPotatoLord <918780@dpsk12.net>
2022-05-23 10:21:35 -0600

    set throttle to 1

--- src/main/java/frc/robot/commands/Spin.java
+++ src/main/java/frc/robot/commands/Spin.java
@@ -39,7 +39,7 @@ public class Spin extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-		m_driveBase.driveCartesian(0, 0, 0.5); 
+		m_driveBase.driveCartesian(0, 0, 0.5,1); 
 	}
 
   // Called once the command ends or is interrupted.
