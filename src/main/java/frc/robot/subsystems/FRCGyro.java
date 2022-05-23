50397b118a4c96727d8f13b3b7079a5b230d89eb
bakedPotatoLord <918780@dpsk12.net>
2022-05-23 12:51:56 -0600

    rename to FRCGyro

--- src/main/java/frc/robot/subsystems/FRCGyro.java
+++ src/main/java/frc/robot/subsystems/FRCGyro.java
@@ -3,14 +3,20 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Gyro extends SubsystemBase{
+public class FRCGyro extends SubsystemBase {
 
   AnalogGyro m_gyro = new AnalogGyro(0);
 
-  public Gyro() {
+  public FRCGyro() {
     m_gyro.calibrate();
-      
+    
   }
+
+  public double getAngle(){
+    return m_gyro.getAngle();
+  }
+
+
     
 }
 
