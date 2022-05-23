8e3e55986c8696ce28274ab2d700fca4cec99815
bakedPotatoLord <918780@dpsk12.net>
2022-05-23 10:00:44 -0600

    added deadbands

--- src/main/java/frc/robot/subsystems/DriveBase.java
+++ src/main/java/frc/robot/subsystems/DriveBase.java
@@ -5,20 +5,18 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.MathUtil;
+
 //import frc.robot.RobotContainer;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
+
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+
 public class DriveBase extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public DriveBase() {
-    m_fr.setInverted(true);
-    m_rr.setInverted(true);
-  }
-
-  
 
   
   private final CANSparkMax m_fl = new CANSparkMax(5,MotorType.kBrushed);
@@ -26,10 +24,20 @@ public class DriveBase extends SubsystemBase {
   private final CANSparkMax m_fr = new CANSparkMax(7,MotorType.kBrushed);
   private final CANSparkMax m_rr = new CANSparkMax(8,MotorType.kBrushed);
 
-  private final MecanumDrive m_drive = new MecanumDrive(m_fl,m_fr,m_rl,m_rr);
+  private final MecanumDrive m_drive;
   
-  public void driveCartesian(double y,double x,double z){
-    m_drive.driveCartesian(y, x, z, 0.0);
+  public DriveBase() {
+
+    m_fr.setInverted(true);
+    m_rr.setInverted(true);
+    m_drive = new MecanumDrive(m_fl,m_rl,m_fr,m_rr);
   }
 
+  public void driveCartesian(double y,double x,double z){
+    Double yd=MathUtil.applyDeadband(y, 0.1);
+    Double xd=MathUtil.applyDeadband(x, 0.1);
+    Double zd=MathUtil.applyDeadband(z, 0.25);
+
+    m_drive.driveCartesian(yd, -xd, -zd, 0.0);
+  }
 }
