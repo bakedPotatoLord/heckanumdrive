647c476e181c26d733095841785679ddde2dc931
BakedPotatoLord <918780@dpsk12.net>
2022-05-25 20:30:36 -0600

    put throttle at 1

--- src/main/java/frc/robot/commands/Test.java
+++ src/main/java/frc/robot/commands/Test.java
@@ -4,15 +4,17 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.*;
 
 public class Test extends WaitCommand {
-    DriveBase driveBase = new DriveBase();
-    public Test(){
+    DriveBase driveBase;
+    public Test(DriveBase subsystem){
         super(2);
+        
+        driveBase = subsystem;
         addRequirements(driveBase);
     }
 
     @Override
     public void execute() {
  
-        driveBase.driveCartesian(0.5,0,0,0);
+        driveBase.driveCartesian(0.5,0,0,1);
     }
 }
